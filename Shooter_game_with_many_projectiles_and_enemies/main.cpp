fd102d337bb9cc85a30b3144db94a7e7f7e18955
Johnathan Johnson <Johnathan.D.Johnson803@gmail.com>
2024-04-20 01:10:29 -0500

    Finished building the simple shooter game with many projectiles. Added the collision detection system.

--- Shooter_game_with_many_projectiles_and_enemies/main.cpp
+++ Shooter_game_with_many_projectiles_and_enemies/main.cpp
@@ -4,7 +4,7 @@
 #include "SFML/System.hpp"
 
 #include <vector>
-
+#include <cstdlib>
 
 int main()
 {
@@ -20,6 +20,10 @@ int main()
 	sf::RectangleShape enemy;
 	enemy.setFillColor(sf::Color::Magenta);
 	enemy.setSize(sf::Vector2f(50.f, 50.f));
+	std::vector<sf::RectangleShape> enemies;
+	enemies.push_back(sf::RectangleShape(enemy));
+    int enemySpawnTimer = 0;
+
 
 	sf::CircleShape player;
 	player.setFillColor(sf::Color::White);
@@ -30,9 +34,6 @@ int main()
 
 	std::vector<sf::CircleShape> projectiles;
 	projectiles.push_back(sf::CircleShape(projectile));
-
-	std::vector<sf::RectangleShape> enemies;
-	enemies.push_back(sf::RectangleShape(enemy));
 	
 	while(window.isOpen())
 	{
@@ -76,6 +77,46 @@ int main()
 		for(size_t i=0; i < projectiles.size(); i++)
 		{
 			window.draw(projectiles[i]);
+		}
+
+		 //ENEMIES
+        if(enemySpawnTimer < 20)
+        {
+            enemySpawnTimer++;
+        }
+
+        if(enemySpawnTimer >= 20)
+        {   
+            enemy.setPosition(rand() % window.getSize().x - enemy.getSize().x, -50.f);
+            enemies.push_back(sf::RectangleShape(enemy));
+            enemySpawnTimer = 0;
+        }
+
+        for(size_t i=0; i<enemies.size(); i++)
+        {
+				enemies[i].move(0, 1.f);
+				if(enemies[i].getPosition().y > window.getSize().y)
+				{
+					enemies.erase(enemies.begin() + i);
+				}
+        }
+
+		for(size_t i=0; i<enemies.size(); i++)
+		{
+			window.draw(enemies[i]);
+		}
+
+		//COLLISION
+		for(size_t i=0; i < projectiles.size(); i++)
+		{
+			for(size_t j=0; j<enemies.size(); j++)
+			{
+				if(projectiles[i].getGlobalBounds().intersects(enemies[j].getGlobalBounds()))
+				{
+					projectiles.erase(projectiles.begin() + i);
+					enemies.erase(enemies.begin() + j);
+				}
+			}
 		}
 		
 		window.display();
