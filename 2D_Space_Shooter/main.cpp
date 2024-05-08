9c6a0c97eabdc902b5c1dd9a9ca3fe9b82dbb59a
Johnathan Johnson <Johnathan.D.Johnson803@gmail.com>
2024-05-08 11:37:11 -0500

    added window collision, player to enemy collision, and enemy to bullet collision

--- 2D_Space_Shooter/main.cpp
+++ 2D_Space_Shooter/main.cpp
@@ -4,70 +4,160 @@
 #include <SFML/Window.hpp>
 #include <math.h>
 #include <cstdlib>
+#include <vector>
 
 class Player
 {
-public:
-    sf::Sprite shape;
-    sf::Texture* texture;
-    int HP;
-    int HPMax;
+	public:
+		sf::Sprite player;
 
-    Player(sf::Texture *texture)
-    {   
-        this->HPMax = 10;
-        this->HP = this->HPMax;
+		Player(sf::Texture *texture)
+		{
+			this->player.setTexture(*texture);
+			this->player.setScale(0.2, 0.2);
+			this->player.setRotation(360);
 
-        this->texture = texture;
-        this->shape.setTexture(*texture);
-        this->shape.setScale(0.1f, 0.1f);
-    }
+		}
 
-    ~Player(){};
-
-};
-
-class Enemy
-{
-    public:
-
+		~Player(){};
 };
 
 class Bullet
 {
-    public:
+	public:
+	sf::Sprite bullet;
 
+	Bullet(sf::Texture *texture, Player playerPosition)
+	{	
+		this->bullet.setTexture(*texture);
+		this->bullet.setScale(0.1f, 0.1f);
+		this->bullet.setPosition(playerPosition.player.getPosition().x + (playerPosition.player.getGlobalBounds().width / 2), playerPosition.player.getPosition().y + (playerPosition.player.getGlobalBounds().height / 2));
+	}
+
+	~Bullet(){};
+};
+
+class Enemy
+{
+	public:
+		sf::Sprite enemy;
+		
+		Enemy(sf::Texture *texture, sf::Vector2u windowSize)
+		{
+			this->enemy.setTexture(*texture);
+			this->enemy.setScale(0.2, 0.2);
+
+			this->enemy.setPosition(windowSize.x, rand() % (windowSize.y - static_cast<int>(this->enemy.getGlobalBounds().height)));
+			
+		}
+		~Enemy(){};
 };
 
 int main()
 {
-    srand(time(NULL));
+	srand(time(NULL));
+	sf::RenderWindow window(sf::VideoMode(1920,1080),"Space Shooter");
+	window.setFramerateLimit(60);
+	sf::Event event;
 
-    sf::RenderWindow window(sf::VideoMode(1920, 1080), "2D Space Game");
-    window.setFramerateLimit(60);
+	//Texture Init
+	//Player Texture
+	sf::Texture playerTexture;
+	playerTexture.loadFromFile("textures/ship.png");
+	Player p1(&playerTexture);
 
-    //Init Textures
-    sf::Texture playerTexture;
-    if(!playerTexture.loadFromFile("textures/ship.png"))
-    {
-        throw("Could not load Player Texture!");
-    }
+	//Enemy
+	//Texture
+	sf::Texture enemyTexture;
+	enemyTexture.loadFromFile("textures/enemy1.png");
+	std::vector<Enemy> enemies;
+	enemies.push_back(Enemy(&enemyTexture, window.getSize()));
+	int enemyTimer = 20;
 
-    Player player(&playerTexture);
+	//Bullet 
+	sf::Texture bulletTexture;
+	bulletTexture.loadFromFile("textures/missileTex01.png");
+	std::vector<Bullet> missles;
 
-    sf::Event event;
+	while(window.isOpen())
+	{
+		while(window.pollEvent(event))
+		{
+			if(event.type == sf::Event::Closed)
+				window.close();
+		}
 
-    while(window.isOpen())
-    {
-        while(window.pollEvent(event))
-        {
-            if(event.type == sf::Event::Closed)
-                window.close();
-        }
+		//Player Movement 
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+			p1.player.move(0, -5.f);
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+			p1.player.move(0, 5.f);
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+			p1.player.move(-5.f, 0);
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+			p1.player.move(5.f, 0);
 
-        //Update 
+		window.clear();
 
-        //Draw
-        window.draw(player.shape);
-    }
+		//Firing Missle
+		if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
+			{
+				missles.push_back(Bullet(&bulletTexture, p1));
+			}
+		
+		//Player collision with window borders
+		if(p1.player.getPosition().x <= 0) //Left border of the window
+			p1.player.setPosition(0, p1.player.getPosition().y);
+
+		if(p1.player.getPosition().x + p1.player.getGlobalBounds().width >= window.getSize().x) //Right border of the window 
+			p1.player.setPosition(window.getSize().x - p1.player.getGlobalBounds().width, p1.player.getPosition().y);
+		
+		if(p1.player.getPosition().y <= 0) //Top bprder of the window
+			p1.player.setPosition(p1.player.getPosition().x, 0);
+		
+		if(p1.player.getPosition().y + p1.player.getGlobalBounds().height >= window.getSize().y) //Bottom of the window
+			p1.player.setPosition(p1.player.getPosition().x, window.getSize().y - p1.player.getGlobalBounds().height);
+		
+		
+		
+		//Enemy 
+		//Enemy Spawn Timer
+		if(enemyTimer <= 100)
+		{
+			enemyTimer++;
+		}
+
+		if(enemyTimer >= 100)
+		{
+			enemies.push_back(Enemy(&enemyTexture, window.getSize()));
+			enemyTimer = 0;
+		}
+
+		//Drawing and Moving enemies
+		for(size_t i=0; i<enemies.size(); i++)
+		{
+			enemies[i].enemy.move(-5.f, 0);
+			window.draw(enemies[i].enemy);
+			//Enemy Collision with border and player 
+			if(enemies[i].enemy.getPosition().x + enemies[i].enemy.getGlobalBounds().width <= 0 || enemies[i].enemy.getGlobalBounds().intersects(p1.player.getGlobalBounds()))
+				enemies.erase(enemies.begin() + i);
+		}
+
+		for(size_t i=0; i<missles.size(); i++)
+		{
+			missles[i].bullet.move(20.f, 0);
+			window.draw(missles[i].bullet);
+			if(enemies[i].enemy.getGlobalBounds().intersects(missles[i].bullet.getGlobalBounds()))
+			{
+				enemies.erase(enemies.begin() + i);
+				missles.erase(missles.begin() + i);
+			}
+		}
+
+		
+		window.draw(p1.player);
+		window.display();
+	}
+
+	return 0;
 }
